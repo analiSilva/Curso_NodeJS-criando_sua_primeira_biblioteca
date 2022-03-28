4fbbc91f3f030ae2473230dfa61e57975691bcf6
Anali <anali.silva@direct.one>
2022-03-28 19:15:13 -0300

    feat: foi trocado todos console.log por return nas funções e tratado o return da variável arrayResultados

--- index.js
+++ index.js
@@ -10,23 +10,21 @@ function extraiLinks(texto){
     arrayResultados.push({ [temp[1]]: temp[2] })
   }
 
-  return(arrayResultados)
+  return arrayResultados == 0 ? 'não há links' : arrayResultados;
 }
 
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
-async function pegaArquivo(caminhoArquivo){
+async function pegaArquivo(caminhoDoArquivo){
   const encoding = "utf-8";
   try{
-    const texto = await fs.promises.readFile(caminhoArquivo, encoding)
-    console.log(extraiLinks(texto))
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    return extraiLinks(texto);
   } catch (erro) {
     trataErro(erro)
   } 
 }
-
-//pegaArquivo('./arquivos/texto1.md')
 
 module.exports = pegaArquivo;
