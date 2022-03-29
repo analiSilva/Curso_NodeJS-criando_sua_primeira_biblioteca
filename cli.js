84ad45d7e382fbd13631bf5198ecdfcecedd673e
Anali <anali.silva@direct.one>
2022-03-29 19:13:29 -0300

    feat: realizado uma função que valida o status da URL, quando passado a opção validar

--- cli.js
+++ cli.js
@@ -1,10 +1,15 @@
 const chalk = require('chalk');
 const pegaArquivo = require('./index');
 const caminho = process.argv;
+const validaUrls = require('./http-validacao');
 
 async function processaTexto(caminhoDoArquivo){
     const resultado = await pegaArquivo(caminhoDoArquivo[2]);
-    console.log(chalk.yellow('lista de links'), resultado);
+    if(caminho[3] === "validar"){
+        console.log(chalk.yellow('links validados'), await validaUrls(resultado))
+    } else {
+        console.log(chalk.yellow('lista de links'), resultado);
+    }
 }
 
 processaTexto(caminho);
