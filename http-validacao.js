109d4e018ffea113c07ebe94ac8543e009562917
Anali <anali.silva@direct.one>
2022-03-29 19:19:06 -0300

    feat: criado uma função de valida as URLs, utilizando a lib fetch para retornar o status.code e o status.text e o método promise.all para acessar de maneira assíncrona o retorno de array de URLs e receber um resultado

---
+++ http-validacao.js
@@ -0,0 +1,36 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+function manejaErros(erro){
+    throw new Error(erro.message);
+}
+async function checaStatus(arrayUrls) {
+    try{
+        const arrayStatus = await Promise
+        .all(arrayUrls
+            .map(async url => {
+                const res = await fetch(url)
+                return `${res.status} - ${res.statusText}`;
+        }));
+        return arrayStatus;
+    } catch(erro){
+        manejaErros(erro)
+    }
+} 
+    
+function geraArrayUrls(arrayLinks){
+   return arrayLinks
+    .map(objetoLink => Object
+        .values(objetoLink).join())
+}
+async function validaUrls(arrayLinks){
+    const links =  geraArrayUrls(arrayLinks);
+    const statusLinks = await checaStatus(links);
+    //spread operator(...)
+    const resultados = arrayLinks.map((objeto, indice) => ({
+        ...objeto, 
+        status: statusLinks[indice]
+    }) )
+    return resultados;
+}
+
+module.exports = validaUrls;
